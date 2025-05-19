2f3beabb56be1adabbca7b833d36e0090667294f
pretheevi <132347868+pretheevi@users.noreply.github.com>
2025-05-19 18:07:28 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://personal-contact-book-server.onrender.com',
+    baseURL: 'https://personalcontactbookserver-production.up.railway.app',
     headers: {
         'Content-Type': 'application/json'
     }
